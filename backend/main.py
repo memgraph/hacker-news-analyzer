c6579c9f333c5e3c21d38409a1745eb2c1f88a22
Lucija Perkovic <lucijaperkovic@gmail.com>
2022-07-19 13:07:29 +0200

    started Docker

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,19 @@
-import re
-from fastapi import FastAPI
+from typing import Union
 
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
 
 
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
 
+if __name__ == '__main__':
+    uvicorn.run(app)
