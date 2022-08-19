d5a0e892d24f70a218e8f187c0629814a6f3f59b
Lucija Perkovic <lucijaperkovic@gmail.com>
2022-08-19 09:28:34 +0200

    Delete print

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,6 @@
-
-from functools import wraps
-import logging
-import os
-import time
-from unicodedata import name
 from gqlalchemy import Memgraph, MemgraphKafkaStream
-from time import sleep
 
-
-log = logging.getLogger(__name__)
 memgraph = Memgraph()
-
-
-def log_time(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        duration = time.time() - start_time
-        log.info(f"Time for {func.__name__} is {duration}")
-        return result
-    return wrapper
-
-def init_log():
-    logging.basicConfig(level=logging.DEBUG)
-    log.info("Logging enabled")
-    logging.getLogger("werkzeug").setLevel(logging.WARNING)
-
-def connect_to_memgraph():
-    connection_established = False
-    while not connection_established:
-        if memgraph._get_cached_connection().is_active():
-            connection_established = True
 
 def main():
     stream = MemgraphKafkaStream(
@@ -42,7 +11,6 @@ def main():
     )
     memgraph.create_stream(stream)
     memgraph.start_stream(stream)
-    print(memgraph.get_streams())
 
 
 if __name__ == '__main__':
